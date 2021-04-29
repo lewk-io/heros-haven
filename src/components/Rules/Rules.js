027a408372899745d4c7becd36d6809b72391b9d
lewk-io <lukemeaden+github@gmail.com>
2021-04-29 18:46:16 +0100

    Update monetization text

--- src/components/Rules/Rules.js
+++ src/components/Rules/Rules.js
@@ -29,13 +29,14 @@ class Rules extends Component {
                                 <h2 className="monetization__header">Monetization:<a id="monetization"></a></h2>
                             </div>
                             <div className="toast-body monetization__body">
-                                <p>You are able to donate to us <a href="https://discord.gg/PhMeX4zt47" target="_blank">in Discord</a> or via <a href="https://www.patreon.com/hhdayz" target="_blank">Patreon</a>.</p>
+                                <p>You are able to donate to us <a href="https://discord.gg/PhMeX4zt47" target="_blank">in Discord</a> or via <a href="https://www.patreon.com/hhdayz" target="_blank">Patreon</a>.<br />
+                                <i>Prices varry if you donate through Discord or Patreon.</i></p>
                                 By donating, you are accepting the following:
                                 <ul>
                                     <li>the payment method used is either; yours or the owner has given you permission to use it.</li>
                                     <li>all donations are final. Failure to follow our rules (<a href="#server-rules">below</a>) is breaking our terms of service and may result in a perminant or temporary ban.</li>
                                 </ul>
-                                <p><strong>Monetization is currently limited to priority queue only.</strong></p>
+                                <p><strong>Monetization is currently limited to priority queue and Discord donator channel access only.</strong></p>
                             </div>
                         </div>
                     </div>
