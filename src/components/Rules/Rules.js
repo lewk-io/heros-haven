91071ba5a71d677d94c86e8ab2fbce2cd19e5305
lewk-io <lukemeaden+github@gmail.com>
2021-04-29 17:44:17 +0100

    Styling and structure changes

--- src/components/Rules/Rules.js
+++ src/components/Rules/Rules.js
@@ -7,31 +7,34 @@ class Rules extends Component {
         return (
             <div className="container">
                 <div className="row">
-                    <div className="col-12">
-                        <h2>Monetization:<a id="monetization"></a></h2>
-                        <p>You are able to donate to us <a href="https://discord.gg/PhMeX4zt47" target="_blank">in Discord</a> or via <a href="https://www.patreon.com/hhdayz" target="_blank">Patreon</a>.</p>
-                        <p>By donating, you are accepting the following:
-                            <ul>
-                                <li>the payment method used is either; yours or you have been given permission to use it by the owner.</li>
-                                <li>all donations are final. Failure to follow our rules (<a href="#server-rules">below</a>) is breaking our terms of service and may result in a perminant or temporary ban.</li>
-                            </ul>
-                        </p>
+                    <div className="col-sm col-sm-8">
+                        <div className="server-rules">
+                            <h2>Server rules:<a id="server-rules"></a></h2>
+                            <div dangerouslySetInnerHTML={{ __html: formatDiscoHook(RULES.content) }} />
+                            { RULES.embeds.map((e, i) => <div key={i}>
+                                    <h3>{ e.title }</h3>
+                                    <div dangerouslySetInnerHTML={{ __html: formatDiscoHook(e.description) }} />
+                            </div>) }
+                        </div>
+                    </div>
+                    <div className="col-sm col-sm-4">
+                        <div className="toast monetization">
+                            <div class="toast-header">
+                                <h2 className="monetization__header">Monetization:<a id="monetization"></a></h2>
+                            </div>
+                            <div className="toast-body monetization__body">
+                                <p>You are able to donate to us <a href="https://discord.gg/PhMeX4zt47" target="_blank">in Discord</a> or via <a href="https://www.patreon.com/hhdayz" target="_blank">Patreon</a>.</p>
+                                <p>By donating, you are accepting the following:
+                                    <ul>
+                                        <li>the payment method used is either; yours or the owner has given you permission to use it.</li>
+                                        <li>all donations are final. Failure to follow our rules (<a href="#server-rules">below</a>) is breaking our terms of service and may result in a perminant or temporary ban.</li>
+                                    </ul>
+                                </p>
+                                <p><strong>Monetization is currently limited to priority queue only.</strong></p>
+                            </div>
+                        </div>
                     </div>
                 </div>
-                <div className="row">
-                    <div className="col-12">
-                        <h2>Server rules:<a id="server-rules"></a></h2>
-                    </div>
-                </div>
-                <div className="row">
-                    <div className="col-12" dangerouslySetInnerHTML={{ __html: formatDiscoHook(RULES.content) }} />
-                </div>
-                { RULES.embeds.map((e, i) => <div className="row" key={i}>
-                    <div className="col-12">
-                        <h3>{ e.title }</h3>
-                    </div>
-                    <div className="col-12" dangerouslySetInnerHTML={{ __html: formatDiscoHook(e.description) }} />
-                </div>) }
             </div>
         )
     }
